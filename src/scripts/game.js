385f49f410e2c3e8175af49fb62edd48272d411c
exslym <76844562+exslym@users.noreply.github.com>
2023-04-03 14:33:15 +0400

    added tiles update logics

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -10,4 +10,64 @@ export const game = function setGame() {
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ];
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement('div');
+      tile.id = r.toString() + '-' + c.toString();
+      let num = board[r][c];
+      updateTile(tile, num);
+      document.getElementById('board').append(tile);
+    }
+  }
+
+  //create 2 to begin the game
+  setTwo();
+  setTwo();
 };
+
+function updateTile(tile, num) {
+  tile.innerText = '';
+  tile.classList.value = ''; //clear the classList
+  tile.classList.add('tile');
+  if (num > 0) {
+    tile.innerText = num.toString();
+    if (num <= 4096) {
+      tile.classList.add('x' + num.toString());
+    } else {
+      tile.classList.add('x8192');
+    }
+  }
+}
+
+function setTwo() {
+  if (!hasEmptyTile()) {
+    return;
+  }
+  let found = false;
+  while (!found) {
+    //find random row and column to place a 2 in
+    let r = Math.floor(Math.random() * rows);
+    let c = Math.floor(Math.random() * columns);
+    if (board[r][c] == 0) {
+      board[r][c] = 2;
+      let tile = document.getElementById(r.toString() + '-' + c.toString());
+      tile.innerText = '2';
+      tile.classList.add('x2');
+      found = true;
+    }
+  }
+}
+
+function hasEmptyTile() {
+  let count = 0;
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      if (board[r][c] == 0) {
+        //at least one zero in the board
+        return true;
+      }
+    }
+  }
+  return false;
+}
